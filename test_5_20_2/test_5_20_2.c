4147eb20a0ee431d175be2a746eb6408ee52f5f6
luchun <luchunabf@163.com>
2019-05-20 23:58:05 +0800

    日常练习

---
+++ test_5_20_2/test_5_20_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+check_sys()
+{
+ int i =1;
+ return *(char*)(&i);
+
+}
+int main()
+{
+  int ret = check_sys();
+  if(ret == 1)
+    printf("小端\n");
+  else 
+  {
+    printf("大端\n");
+  }
+  return 0;
+    
+}
