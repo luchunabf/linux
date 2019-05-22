ebfad9d983634b58b04a748e801f15c006290686
luchun <luchunabf@163.com>
2019-05-22 23:53:32 +0800

    日常练习

---
+++ test_5_22_1/test_5_22_1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void Swap(int*a, int*b)
+{
+  int tmp;
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+void Bubblesort( int*arr, int size )
+{
+  int i = 0;
+  for(i = 0; i < size-1; i++)
+  {
+    int Flag = 0;
+    int j = 0;
+    for(j = 0;j < size-1-i; j++)
+    {
+        if(arr[j] > arr[j+1])
+        {
+          Swap(&arr[j],&arr[j+1]);
+          Flag = 1;
+        }
+    }
+    if(Flag == 0)
+      return;
+}
+int main()
+{
+
+
+  int arr[9] = { 87, 94, 66, 78, 45, 39, 76, 8, 10 };
+  int size = 9;
+  Bubblesort(arr,size);
+  int i = 0;
+  for(i = 0; i < size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+  return 0;
+}
