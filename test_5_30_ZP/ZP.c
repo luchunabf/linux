7861e08a4a06b8d4e37b94a082b3be725f618849
luchun <luchunabf@163.com>
2019-05-30 23:44:47 +0800

    僵尸进程

---
+++ test_5_30_ZP/ZP.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  pid_t id = fork();
+  if(id < 0)
+  {
+    perror("fork");
+    return 1;
+  }
+  else if(id > 0)
+  {
+    //parent
+    printf("parent[%d] is sleeping...\n", getpid());
+    sleep(30);
+  }
+  else 
+  {
+    //child
+    printf("child[%d] is begin Z...\n", getpid());
+    sleep(5);
+    exit(EXIT_SUCCESS);
+  }
+  return 0;
+}
